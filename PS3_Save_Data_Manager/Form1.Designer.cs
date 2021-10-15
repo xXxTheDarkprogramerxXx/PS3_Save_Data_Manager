a1fbd8fded85ac95aa9ae825b267d120bdd5c581
xXxTheDarkprogramerxXx <eonvwyk@gmail.com>
2021-10-15 09:40:18 +0200

    Created new Repo

---
+++ PS3_Save_Data_Manager/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace PS3_Save_Data_Manager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewImageColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select a directory:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(140, 6);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(662, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(808, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "...";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.dataGridView1.Location = new System.Drawing.Point(15, 56);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 40;
+            this.dataGridView1.Size = new System.Drawing.Size(862, 298);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "GAME ID";
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Title";
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Account Info";
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Image";
+            this.Column4.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Zoom;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Background";
+            this.Column5.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Zoom;
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Details";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Resizable = System.Windows.Forms.DataGridViewTriState.True;
+            this.Column6.SortMode = System.Windows.Forms.DataGridViewColumnSortMode.NotSortable;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(889, 366);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewImageColumn Column4;
+        private System.Windows.Forms.DataGridViewImageColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+    }
+}
+
---
+++ PS3_Save_Data_Manager/Form1.cs
@@ -0,0 +1,115 @@
+﻿/*
+                                                                                                                                                                                                                                              
+                                                                                                                                                                                                                                             
+MMMMMMMM               MMMMMMMM               AAA               DDDDDDDDDDDDD      EEEEEEEEEEEEEEEEEEEEEE     BBBBBBBBBBBBBBBBB   YYYYYYY       YYYYYYY     XXXXXXX       XXXXXXXDDDDDDDDDDDDD      PPPPPPPPPPPPPPPPP   XXXXXXX       XXXXXXX
+M:::::::M             M:::::::M              A:::A              D::::::::::::DDD   E::::::::::::::::::::E     B::::::::::::::::B  Y:::::Y       Y:::::Y     X:::::X       X:::::XD::::::::::::DDD   P::::::::::::::::P  X:::::X       X:::::X
+M::::::::M           M::::::::M             A:::::A             D:::::::::::::::DD E::::::::::::::::::::E     B::::::BBBBBB:::::B Y:::::Y       Y:::::Y     X:::::X       X:::::XD:::::::::::::::DD P::::::PPPPPP:::::P X:::::X       X:::::X
+M:::::::::M         M:::::::::M            A:::::::A            DDD:::::DDDDD:::::DEE::::::EEEEEEEEE::::E     BB:::::B     B:::::BY::::::Y     Y::::::Y     X::::::X     X::::::XDDD:::::DDDDD:::::DPP:::::P     P:::::PX::::::X     X::::::X
+M::::::::::M       M::::::::::M           A:::::::::A             D:::::D    D:::::D E:::::E       EEEEEE       B::::B     B:::::BYYY:::::Y   Y:::::YYY     XXX:::::X   X:::::XXX  D:::::D    D:::::D P::::P     P:::::PXXX:::::X   X:::::XXX
+M:::::::::::M     M:::::::::::M          A:::::A:::::A            D:::::D     D:::::DE:::::E                    B::::B     B:::::B   Y:::::Y Y:::::Y           X:::::X X:::::X     D:::::D     D:::::DP::::P     P:::::P   X:::::X X:::::X   
+M:::::::M::::M   M::::M:::::::M         A:::::A A:::::A           D:::::D     D:::::DE::::::EEEEEEEEEE          B::::BBBBBB:::::B     Y:::::Y:::::Y             X:::::X:::::X      D:::::D     D:::::DP::::PPPPPP:::::P     X:::::X:::::X    
+M::::::M M::::M M::::M M::::::M        A:::::A   A:::::A          D:::::D     D:::::DE:::::::::::::::E          B:::::::::::::BB       Y:::::::::Y               X:::::::::X       D:::::D     D:::::DP:::::::::::::PP       X:::::::::X     
+M::::::M  M::::M::::M  M::::::M       A:::::A     A:::::A         D:::::D     D:::::DE:::::::::::::::E          B::::BBBBBB:::::B       Y:::::::Y                X:::::::::X       D:::::D     D:::::DP::::PPPPPPPPP         X:::::::::X     
+M::::::M   M:::::::M   M::::::M      A:::::AAAAAAAAA:::::A        D:::::D     D:::::DE::::::EEEEEEEEEE          B::::B     B:::::B       Y:::::Y                X:::::X:::::X      D:::::D     D:::::DP::::P                X:::::X:::::X    
+M::::::M    M:::::M    M::::::M     A:::::::::::::::::::::A       D:::::D     D:::::DE:::::E                    B::::B     B:::::B       Y:::::Y               X:::::X X:::::X     D:::::D     D:::::DP::::P               X:::::X X:::::X   
+M::::::M     MMMMM     M::::::M    A:::::AAAAAAAAAAAAA:::::A      D:::::D    D:::::D E:::::E       EEEEEE       B::::B     B:::::B       Y:::::Y            XXX:::::X   X:::::XXX  D:::::D    D:::::D P::::P            XXX:::::X   X:::::XXX
+M::::::M               M::::::M   A:::::A             A:::::A   DDD:::::DDDDD:::::DEE::::::EEEEEEEE:::::E     BB:::::BBBBBB::::::B       Y:::::Y            X::::::X     X::::::XDDD:::::DDDDD:::::DPP::::::PP          X::::::X     X::::::X
+M::::::M               M::::::M  A:::::A               A:::::A  D:::::::::::::::DD E::::::::::::::::::::E     B:::::::::::::::::B     YYYY:::::YYYY         X:::::X       X:::::XD:::::::::::::::DD P::::::::P          X:::::X       X:::::X
+M::::::M               M::::::M A:::::A                 A:::::A D::::::::::::DDD   E::::::::::::::::::::E     B::::::::::::::::B      Y:::::::::::Y         X:::::X       X:::::XD::::::::::::DDD   P::::::::P          X:::::X       X:::::X
+MMMMMMMM               MMMMMMMMAAAAAAA                   AAAAAAADDDDDDDDDDDDD      EEEEEEEEEEEEEEEEEEEEEE     BBBBBBBBBBBBBBBBB       YYYYYYYYYYYYY         XXXXXXX       XXXXXXXDDDDDDDDDDDDD      PPPPPPPPPP          XXXXXXX       XXXXXXX
+                                                                                                                                                                                                                                             
+                                                                                                                                                                                                                                             
+                                                                                                                                                                                                                                             
+                                                                                                                                                                                                                                             
+                                                                                                                                                                                                                                             
+                                                                                                                                                                                                                                             
+                                                                                                                                                                                                                                             
+ 
+ 
+ 
+ */
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PS3_Save_Data_Manager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Param_SFO.PARAM_SFO> Params = new List<Param_SFO.PARAM_SFO>();
+
+   
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog folderBrowser = new OpenFileDialog();
+            // Set validate names and check file exists to false otherwise windows will
+            // not let you select "Folder Selection."
+            folderBrowser.ValidateNames = false;
+            folderBrowser.CheckFileExists = false;
+            folderBrowser.CheckPathExists = true;
+            // Always default to Folder Selection.
+            folderBrowser.FileName = "Folder Selection.";
+            dataGridView1.Rows.Clear();
+            if (folderBrowser.ShowDialog() == DialogResult.OK)
+            {
+                string folderPath = Path.GetDirectoryName(folderBrowser.FileName);
+                textBox1.Text = folderPath;
+                var items = Directory.GetFiles(folderPath, "*.SFO", SearchOption.AllDirectories);
+                for (int i = 0; i < items.Length; i++)
+                {
+                    Param_SFO.PARAM_SFO sfo = new Param_SFO.PARAM_SFO(items[i]);
+                    Params.Add(sfo);
+                    //GET BLUES
+                    string SaveDataDir = "";
+
+                    Bitmap Icon = null;
+                    Bitmap Background = null;
+                    string PathToItem = Path.GetDirectoryName(items[i]);
+                    if(File.Exists(PathToItem + @"\ICON0.PNG"))
+                    {
+                        Icon =new Bitmap(PathToItem + @"\ICON0.PNG");
+                    }
+                    if (File.Exists(PathToItem + @"\PIC1.PNG"))
+                    {
+                        Background = new Bitmap(PathToItem + @"\PIC1.PNG");
+                    }
+
+                    dataGridView1.Rows.Add(sfo.GetSpesificValue("SAVEDATA_DIRECTORY").Substring(0, 9), sfo.Title, sfo.GetSpesificValue("ACCOUNT_ID"), Icon, Background, sfo.GetSpesificValue("DETAIL"));
+                }
+            } 
+            
+        }
+    }
+
+
+    public static class OverrideMethod
+    {
+        public static string GetSpesificValue(this Param_SFO.PARAM_SFO psfo, string ValueToFind)
+        {
+            for (int i = 0; i < psfo.Tables.Count; i++)
+            {
+                if (psfo.Tables[i].Name == ValueToFind)
+                {
+                    //get the value 
+                    return psfo.Tables[i].Value;
+                }
+            }
+            return null;
+        }
+    }
+}
